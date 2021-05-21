e00097d3f9873859ea0f632f6726d478dad016dd
RodrigoNiederauer <rodrigonied@hotmail.com>
2021-05-21 10:52:05 -0300

    all test green

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -11,6 +11,12 @@ class CreateUserUseCase {
 
   execute({ email, name }: IRequest): User {
     // Complete aqui
+    const userAlreadyExists = this.usersRepository.findByEmail(email);
+    if (userAlreadyExists) {
+      throw new Error("Email is already used");
+    }
+    const user = this.usersRepository.create({ name, email });
+    return user;
   }
 }
 
